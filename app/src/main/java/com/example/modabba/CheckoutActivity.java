2c268bf77d4c176aa4582e53253d9dc7fea173df
Arpit Mishra <arpitmishra4779@gmail.com>
2019-09-13 21:36:54 +0530

    workflow chips

--- app/src/main/java/com/example/modabba/CheckoutActivity.java
+++ app/src/main/java/com/example/modabba/CheckoutActivity.java
@@ -1,4 +1,14 @@
 package com.example.modabba;
 
-public class CheckoutActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+    }
 }
