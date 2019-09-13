7203bffd6b7e21200a2b996c63eaa860907867a0
Debanshu777 <debanshudatta123@gmail.com>
2019-09-14 01:48:42 +0530

    otp

--- app/src/main/java/com/example/modabba/OtpActivity.java
+++ app/src/main/java/com/example/modabba/OtpActivity.java
@@ -2,13 +2,24 @@ package com.example.modabba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OtpActivity extends AppCompatActivity {
 
+    Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
+        submit=findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(OtpActivity.this,MainActivity.class));
+            }
+        });
     }
 }
