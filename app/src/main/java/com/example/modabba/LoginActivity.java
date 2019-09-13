96dab2e161dfeefea4f29ed334c9db8b7dc5d60d
Debanshu777 <debanshudatta123@gmail.com>
2019-09-14 02:38:17 +0530

    login

--- app/src/main/java/com/example/modabba/LoginActivity.java
+++ app/src/main/java/com/example/modabba/LoginActivity.java
@@ -7,10 +7,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button login;
+    ImageButton login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
