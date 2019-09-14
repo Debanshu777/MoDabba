fe86e2c7b74b41d37c1b6462d568001d55b74c7b
Debanshu777 <debanshudatta123@gmail.com>
2019-09-15 03:15:00 +0530

    login

--- app/src/main/java/com/example/modabba/fragments/Dashboard.java
+++ app/src/main/java/com/example/modabba/fragments/Dashboard.java
@@ -1,6 +1,7 @@
 package com.example.modabba.fragments;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -13,25 +14,35 @@ import android.widget.Button;
 import com.example.modabba.AddressActivity;
 import com.example.modabba.MainActivity;
 import com.example.modabba.R;
+import com.google.android.material.chip.Chip;
 import com.kofigyan.stateprogressbar.StateProgressBar;
 
 public class Dashboard extends Fragment {
     String[] descriptionData = {"Preparing", "On Way", "Delivered"};
     Button sub;
+    Chip non_veg,extra;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v= inflater.inflate(R.layout.fragment_dashboard, container, false);
-        StateProgressBar stateProgressBar = v.findViewById(R.id.your_state_progress_bar_id);
+        StateProgressBar stateProgressBar = v.findViewById(R.id.progress_bar);
         stateProgressBar.setStateDescriptionData(descriptionData);
         sub=v.findViewById(R.id.sub);
+        non_veg=v.findViewById(R.id.Non_veg);
+        extra=v.findViewById(R.id.extra);
         sub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 updateDetail();
             }
         });
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        non_veg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        return v;
     }
     public void updateDetail() {
         Intent intent = new Intent(getActivity(), AddressActivity.class);
